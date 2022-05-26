030238d99fae80867374cb4dea2a838dfda44300
Juho Kontio <98368986+KontioJuho@users.noreply.github.com>
2022-05-26 13:58:37 +0300

    Initial commit

---
+++ R/matrinet_data.R
@@ -0,0 +1,54 @@
+#' @title Test title
+#' @param datalist	 A dataframe or list of group (cancer) dataframes containing target gene variables as column vectors. Additional variables (e.g. sampleIDs) could be included, target genes/proteins are extracted automatically from the dataframe by the column names of prior_topology object
+#' @param quantiles A dataframe or list of cancer-specific gene expression discretization levels for each gene.
+#' @param genenams A character vector of target gene names
+#'
+#' @examples print("matrinet_data(datalist, quantiles, genenames)")
+#' @return A matridata object that can be used as an input for matrinet_estimate function.
+#' @export
+
+
+matrinet_data <- function(datalist, quantiles, genenams){
+
+
+
+
+  continuous_data <- lapply(datalist, function(datalist_tumor){
+    datalist_tumor %>%
+      dplyr::select(genenams)
+  })
+
+  discrete_data <- continuous_data
+  for(i in 1:length(discrete_data)){
+    for(j in colnames(discrete_data[[i]])){
+
+      discrete_data[[i]][,j] <- as.numeric(cut(datalist[[i]][,j],
+                                               breaks = c(-Inf, as.vector(quantiles[[i]][,j]), Inf))) - 2
+
+
+    }}
+  profile_data <- discrete_data
+
+  values <- c(-1,0,1)
+  for(i in 1:length(discrete_data)){
+    profile_data[[i]] <- matrix(0, ncol = ncol(discrete_data[[i]]), nrow = 3)
+    colnames(profile_data[[i]]) <- colnames(discrete_data[[i]])
+    rownames(profile_data[[i]]) <- c("low", "medium", "high")
+
+
+    for(m in colnames(profile_data[[i]])){
+
+      for(k in 1:3){
+
+        profile_data[[i]][k,m] <- length(which(discrete_data[[i]][,m] == values[k]))/nrow(discrete_data[[i]])
+
+      }
+
+    }
+  }
+
+
+  list("continuous" = continuous_data,
+       "discrete" = discrete_data,
+       "profile" = profile_data)
+}
